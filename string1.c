9a2a67335b693de2591feae7602188cda3f344bf
addoray1 <richardbampoe@gmail.com>
2023-12-13 02:36:01 -0800

    simple shell

--- string1.c
+++ string1.c
@@ -76,3 +76,12 @@ int _putchar(char c)
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
